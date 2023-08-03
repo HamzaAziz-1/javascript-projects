8763aa49c92d05462d5f0d41df9c70762d809209
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-08-03 18:23:32 +0500

    Update script.js

--- Image Search App/script.js
+++ Image Search App/script.js
@@ -12,8 +12,8 @@ async function searchImages() {
   inputData = inputElement.value;
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
   const results = data.results;
 
@@ -32,8 +32,8 @@ async function searchImages() {
     imageLink.textContent = result.alt_description;
 
     imageWrapper.appendChild(imageLink);
-   imageLink.appendChild(image);
-   searchResults.appendChild(imageWrapper);
+    imageLink.appendChild(image);
+    searchResults.appendChild(imageWrapper);
   });
 
   page++;
